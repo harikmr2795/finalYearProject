12ab9d009d19d135fbcab5475f27c90eeb58a1d3
harikmr2795 <harikmr2795@gmail.com>
2017-01-06 20:12:47 +0530

    Update testScrapper.py

--- testScrapper.py
+++ testScrapper.py
@@ -1,12 +1,26 @@
 #dependencies
 import feedparser
 
+def check(count):
+    try:
+        x = document['items'][count]
+        return True
+    except Exception:
+        return False
+    return False
+
 document = feedparser.parse('https://www.engadget.com/rss.xml')
 count = 0
-print('Number of topics avoilable in Engadget is ',len(document),'\n')
+print('Number of topics available in Engadget is ',len(document),'\n')
 
 while (count<len(document)):
     element = document['items'][count]
     print(count+1,'. ',element['title'])
     count = count + 1
 
+count = 25
+if(check(count)==True):
+    print("More Available")
+else:
+    print("NotAvailable")
+    
