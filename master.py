39cb5da2c45a184505c6f90f44a3cd8eacb9c1e9
harikmr2795 <harikmr2795@gmail.com>
2017-01-14 11:35:39 +0530

    Scraps and prints Tech News from various websites

--- master.py
+++ master.py
@@ -9,41 +9,58 @@ sourceLinksHTML_H1 = []
 titles = []
 
 try:
-	# RSS Scrapping
-	for link in sourceLinksRSS:
-		print('\nFecthing RSS from',link,'...')
-		x = feedparser.parse(link)
-		for element in x['items']:
-			titles.append(element['title'])
-	# BeautifulSoup Scrapping h3
-	for link in sourceLinksHTML_H3:
-		print('\nFetching News from',link)
-		r  = requests.get(link)
-		html_doc = r.text
-		soup = BeautifulSoup(html_doc,'html.parser')
-		headings = soup.find_all('h3')
-		for heading in headings:
-			titles.append(heading.get_text())
+        # RSS Scrapping
+        for link in sourceLinksRSS:
+                print('\nFecthing RSS from',link,'...')
+                x = feedparser.parse(link)
+                for element in x['items']:
+                        title = element['title']
+                        if(title in titles):
+                                print('\n\n\n')
+                        else:
+                                titles.append(title)
+        # BeautifulSoup Scrapping h3
+        for link in sourceLinksHTML_H3:
+                print('\nFetching News from',link)
+                r  = requests.get(link)
+                html_doc = r.text
+                soup = BeautifulSoup(html_doc,'html.parser')
+                headings = soup.find_all('h3')
+                for heading in headings:
+                        title = heading.get_text()
+                        if(title in titles):
+                                print('\n\n\n')
+                        else:
+                                titles.append(title)
         # BeautifulSoup Scrapping h2
-	for link in sourceLinksHTML_H2:
-		print('\nFetching News from',link)
-		r  = requests.get(link)
-		html_doc = r.text
-		soup = BeautifulSoup(html_doc,'html.parser')
-		headings = soup.find_all('h2')
-		for heading in headings:
-			titles.append(heading.get_text())
-	# BeautifulSoup Scrapping h1
-	for link in sourceLinksHTML_H1:
-		print('\nFetching News from',link)
-		r  = requests.get(link)
-		html_doc = r.text
-		soup = BeautifulSoup(html_doc,'html.parser')
-		headings = soup.find_all('h1')
-		for heading in headings:
-			titles.append(heading.get_text())
+        for link in sourceLinksHTML_H2:
+                print('\nFetching News from',link)
+                r  = requests.get(link)
+                html_doc = r.text
+                soup = BeautifulSoup(html_doc,'html.parser')
+                headings = soup.find_all('h2')
+                for heading in headings:
+                        title = heading.get_text()
+                        if(title in titles):
+                                print('\n\n\n')
+                        else:
+                                titles.append(title)
+        # BeautifulSoup Scrapping h1
+        for link in sourceLinksHTML_H1:
+                print('\nFetching News from',link)
+                r  = requests.get(link)
+                html_doc = r.text
+                soup = BeautifulSoup(html_doc,'html.parser')
+                headings = soup.find_all('h1')
+                for heading in headings:
+                        title = heading.get_text()
+                        if(title in titles):
+                                print('\n\n\n')
+                        else:
+                                titles.append(title)
+                
 except requests.ConnectionError:
-	print('No Internet Connection')
+        print('No Internet Connection')
 
 # To display the parsed titles
 finally:
